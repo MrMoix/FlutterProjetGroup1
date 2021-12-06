c9ca5654c816dc5775b39accbaa96964e0ff1af7
baptisteostertag <baptiste.ostertag@netplus.ch>
2021-12-06 11:11:21 +0100

    dashboard with last tshirt value

--- lib/screens/dataPage.dart
+++ lib/screens/dataPage.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -43,7 +42,7 @@ class _dataPageState extends State<dataPage> {
         allData.add(d);
       }
       setState(() {
-        print('Length : ${allData.length}');
+        print("Data update");
       });
     });
   }
